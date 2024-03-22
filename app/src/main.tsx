94344111078d087b60f451fa41e0b00d4f5706d3
makarovas <makarofalex@gmail.com>
2024-03-22 12:19:38 +0300

    complete migration

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,5 +1,32 @@
-import { render } from 'preact';
-import { AppProvider } from './AppClients.tsx';
+import '@rainbow-me/rainbowkit/styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './global.css';
 import './index.css';
+import './polyfills';
 
-render(<AppProvider />, document.getElementById('app')!);
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import { arbitrum, base, mainnet, optimism, polygon } from 'wagmi/chains';
+import { App } from './app';
+
+const config = getDefaultConfig({
+  appName: 'RainbowKit demo',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+});
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('app')!).render(
+  <React.StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
+);
